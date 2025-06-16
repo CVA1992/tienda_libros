6886edb13bed6d4197bad8b31e4a9ad28ff50dad
Erland Chivas Huanca <erlandchivas3@gmail.com>
2025-06-15 21:04:10 -0400

    mejorar diseño y accesibilidad en resultados de búsqueda y carrito de compras

--- tienda_libros/urls.py
+++ tienda_libros/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from libros.views import inicio
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,5 @@ urlpatterns = [
     path('',inicio, name='inicio'),
     path('logout/', LogoutView.as_view(next_page='inicio'), name='logout'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
